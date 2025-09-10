07f884c629ba98eb18f44cf11bd36cb27f14fe33
AlexanderLislelid <alexlislelid@gmail.com>
2025-09-10 17:41:11 +0200

    added cart and overlay to checkout page

--- js/checkout.js
+++ js/checkout.js
@@ -1,5 +1,31 @@
-import { getCart, clearCart } from "./cart.js";
+import { getCart, clearCart, renderCart } from "./cart.js";
 
+const cartBtn = document.getElementById("cart");
+const overlay = document.getElementById("cart-overlay");
+const closeBtn = document.getElementById("close-cart");
+
+cartBtn.addEventListener("click", () => {
+  overlay.classList.add("open");
+  renderCart();
+});
+
+closeBtn.addEventListener("click", () => {
+  overlay.classList.remove("open");
+});
+
+overlay.addEventListener("click", (e) => {
+  if (e.target === overlay) {
+    overlay.classList.remove("open");
+  }
+});
+
+const clearBtn = document.querySelector("#clear-cart");
+clearBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  clearCart();
+  renderCart();
+  renderCheckout();
+});
 function renderCheckout() {
   const container = document.getElementById("checkout-container");
   const totalDisplay = document.getElementById("checkout-total");
